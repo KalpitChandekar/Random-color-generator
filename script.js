f587293339bb0f39d6e08df06793131ad32cd355
Kalpit Chandekar <kalpitchandekar1736@gmail.com>
2023-08-07 19:29:05 +0530

    add script for color code

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const getColor = () => {
   const randomNumber = Math.floor(Math.random() * 16777215);
   const randomCode = "#" + randomNumber.toString(16);
   document.body.style.backgroundColor = randomCode;
+  document.getElementById('clr-code').innerHTML = randomCode;
 };
 
 document.getElementById("btn").addEventListener("click", getColor);

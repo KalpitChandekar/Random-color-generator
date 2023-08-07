b8754c7cf0fa12676be8cba3b69fc8a351010c65
Kalpit Chandekar <kalpitchandekar1736@gmail.com>
2023-08-07 19:33:07 +0530

    feat: automatically clr code copy in users clipboard

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const getColor = () => {
   const randomCode = "#" + randomNumber.toString(16);
   document.body.style.backgroundColor = randomCode;
   document.getElementById('clr-code').innerHTML = randomCode;
+
+  //automatically code copy to the clipboard
+  navigator.clipboard.writeText(randomCode);
 };
 
 document.getElementById("btn").addEventListener("click", getColor);
